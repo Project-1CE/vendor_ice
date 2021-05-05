37172d1f331ab18a167a630d6511d78ae7717e66
Danny Lin <danny@kdrag0n.dev>
2021-05-05 16:26:49 -0700

    ice: Use Material sounds
    The default AOSP ringtone, alarm, and notification sounds are quite
    lackluster in comparison to the proprietary Pixel sounds. Replace them
    with better ones that are licensed permissively:
      - AOSP Material sounds from Android Pie
        frameworks/base/data/sounds/AudioPackage14.mk
      - Material Product Sounds [1], licensed under CC-BY 4.0
        https://material.io/design/sound/sound-resources.html
    
    Change-Id: I7f1540db9be828543cab61e6e8774b13f64245f1

--- target/product/prebuilts.mk
+++ target/product/prebuilts.mk
@@ -18,3 +18,29 @@
 PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
+
+# Sounds
+$(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
+
+PRODUCT_COPY_FILES += \
+    vendor/ice/sounds/alarms/material_gentle.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/material_gentle.ogg \
+    vendor/ice/sounds/notifications/material_ambient.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_ambient.ogg \
+    vendor/ice/sounds/notifications/material_decorative_1.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_decorative_1.ogg \
+    vendor/ice/sounds/notifications/material_decorative_2.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_decorative_2.ogg \
+    vendor/ice/sounds/notifications/material_decorative-celebration_1.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_decorative-celebration_1.ogg \
+    vendor/ice/sounds/notifications/material_decorative-celebration_2.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_decorative-celebration_2.ogg \
+    vendor/ice/sounds/notifications/material_decorative-celebration_3.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_decorative-celebration_3.ogg \
+    vendor/ice/sounds/notifications/material_high-intensity-alert.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_high-intensity-alert.ogg \
+    vendor/ice/sounds/notifications/material_high-intensity.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_high-intensity.ogg \
+    vendor/ice/sounds/notifications/material_simple_1.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple_1.ogg \
+    vendor/ice/sounds/notifications/material_simple_2.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple_2.ogg \
+    vendor/ice/sounds/notifications/material_simple-alert.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple-alert.ogg \
+    vendor/ice/sounds/notifications/material_simple-celebration_1.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple-celebration_1.ogg \
+    vendor/ice/sounds/notifications/material_simple-celebration_2.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple-celebration_2.ogg \
+    vendor/ice/sounds/notifications/material_simple-celebration_3.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/material_simple-celebration_3.ogg \
+    vendor/ice/sounds/ringtones/material_minimal.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/material_minimal.ogg
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=material_minimal.ogg \
+    ro.config.alarm_alert=material_gentle.ogg \
+    ro.config.notification_sound=material_simple_1.ogg
