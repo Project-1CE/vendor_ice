8da36f3874fe488790ca7190f6b6db8a8ab7e6e9
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:27:57 +0530

    ice: Bring in kernel definition for arm64
    Change-Id: Idc20cb0ade1383c0522201f660efc4c5e7b976e9

--- target/board/BoardConfigKernel.mk
+++ target/board/BoardConfigKernel.mk
@@ -14,5 +14,15 @@
 # limitations under the License.
 #
 
-# Kernel
-include vendor/ice/target/board/BoardConfigKernel.mk
+# Architecture
+TARGET_KERNEL_ARCH := arm64
+
+# Cross compile env
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(shell pwd)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+
+# LLVM support
+KERNEL_LLVM_SUPPORT := true
+
+# Qualcomm kernel
+TARGET_COMPILE_WITH_MSM_KERNEL := true
