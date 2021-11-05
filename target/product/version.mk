f54e218bbfea36ff68f5738ee00bb85c5646a773
alk3pInjection <webmaster@raspii.tech>
2021-11-05 12:42:00 +0800

    ice: define versioning
    Change-Id: Id4adfbfa8806d28cf9772d1883e87ccaf68a0f86

---
+++ target/product/version.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2022 Project ICE
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ICE_CODENAME := sugisawa
+ICE_SUBVERSION := alpha1
+
+ifndef ICE_BUILDTYPE
+  ICE_BUILD_VARIANT := unofficial
+else
+  ICE_BUILD_VARIANT := nightly
+endif
+
+ICE_VERSION := $(ICE_CODENAME)-$(ICE_SUBVERSION)-$(ICE_BUILD)-$(shell date -u +%Y%m%d)-$(shell echo $(ICE_BUILD_VARIANT) | tr a-z A-Z)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ice.version=$(ICE_VERSION)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ice.version.major=$(ICE_CODENAME) \
+    ro.ice.version.minor=$(ICE_SUBVERSION) \
+    ro.ice.build.variant=$(ICE_BUILD_VARIANT)
