2943f8358f48c9e717e47d00bcac571d036cdf8d
alk3pInjection <webmaster@raspii.tech>
2022-04-28 23:07:52 +0800

    products: Promote guacamole to Sv2
    Change-Id: Ifcb105e829924a8e7995ac78625e7c405d3f6221

--- products/guacamole/ice_guacamole.mk
+++ products/guacamole/ice_guacamole.mk
@@ -8,6 +8,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from guacamole device
 $(call inherit-product, device/oneplus/guacamole/device.mk)
@@ -22,6 +23,9 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := GM1910
 PRODUCT_MANUFACTURER := OnePlus
 
+PRODUCT_SYSTEM_NAME := OnePlus7Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus7Pro
+
 PRODUCT_AAPT_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_CHARACTERISTICS := nosdcard
@@ -30,10 +34,10 @@ PRODUCT_CHARACTERISTICS := nosdcard
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:10/QKQ1.190716.003/1909110008:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7Pro/OnePlus7Pro:11/RKQ1.201022.002/2204061044:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=OnePlus7Pro \
-    PRODUCT_NAME=OnePlus7Pro \
-    PRIVATE_BUILD_DESC="OnePlus7Pro-user 10 QKQ1.190716.003 1909110008 release-keys"
+    TARGET_DEVICE=OnePlus7Pro \
+    TARGET_PRODUCT=OnePlus7Pro \
+    PRIVATE_BUILD_DESC="qssi-user 11 RKQ1.201022.002 2204061044 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
