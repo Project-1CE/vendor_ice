07a93c6dd7f637949cd1f8e7447b41d32ab82eaf
Jake Weinstein <jake@aospa.co>
2022-06-07 18:40:33 +0000

    ice: Commonize some telephony framework packages
    These are needed for all devices, not just on Qualcomm platforms.
    
    Change-Id: I7eeeb900e0be27c3e5e219b373b984b57da4da53

--- target/product/packages.mk
+++ target/product/packages.mk
@@ -47,3 +47,34 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor \
     libvndfwk_detect_jni.qti.vendor \
     libqti_vndfwk_detect.vendor
+
+# Telephony - CAF
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml
+
+ifneq ($(TARGET_NO_TELEPHONY), true)
+PRODUCT_PACKAGES += \
+    tcmiface \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml
+
+# Telephony - AOSP
+PRODUCT_PACKAGES += \
+    Stk
+
+PRODUCT_BOOT_JARS += \
+    tcmiface \
+    telephony-ext
+endif
